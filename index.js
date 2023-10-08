78652f0199e1db9d7de932a5ce4e8c566aad3488
aswin chandran <aswin1998ply@gmail.com>
2023-10-08 21:04:10 +0530

    write

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const data = require("./data/data.json");
 const morgan = require("morgan");
-const fs = require('node:fs');
-const { error } = require("node:console");
-
-// const jFile = fs.readFileSync('./data/data.json','utf-8')
-// console.log(jFile);
-
-
+const fs = require("node:fs");
 
 const app = express();
 const PORT = 3030;
@@ -18,18 +12,15 @@ app.use(morgan("dev"));
 //GET-----------
 
 app.get("/hospital", (req, res) => {
+  fs.readFile("./data/data.json", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(JSON.parse(data));
+      console.log(JSON.parse(data));
+    }
+  });
 
-  fs.readFile('./data/data.json','utf-8',(error,data)=>{
-if(error){
-  console.log(error)
-}
-else{
-  res.send(data);
-  console.log(data)
-}
-
-  })
-  // res.json(data);
 });
 
 //POST-----------
@@ -37,6 +28,21 @@ else{
 app.post("/new", (req, res) => {
   data.push(req.body);
   res.send(data);
+
+  const newdata = req.body;
+
+  fs.writeFile(
+    "./data/newdata.json",
+    JSON.stringify(newdata),
+    { flag: "a" },
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("New data successfully written in newdata.json file");
+      }
+    }
+  );
 });
 
 //PUT------------
@@ -55,27 +61,4 @@ app.delete(`/delete`, (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Your server is running on port ${PORT}`);
- 
 });
-
-// // POST Method----------
-
-// router.post('/add',(req,res)=>{
-//   console.log(req.body);
-//   array1.push(req.body);
-//   res.send(array1)
-// })
-
-// // PUT/Update Method-----------
-
-// router.put('/update',(req,res)=>{
-//   console.log(req.body);
-//   array1.splice(0,1,req.body);
-//   res.send(array1)
-// })
-
-// //Deleted Method-----------
-// router.delete('/remove',(req,res)=>{
-//   array1.pop();
-//   res.send(array1)
-// })
